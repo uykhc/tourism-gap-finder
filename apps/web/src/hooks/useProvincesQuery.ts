e31044e0a46a772aacbdcc6d1136c0ac11a5d95c
jun-0411 <jun411@snu.ac.kr>
2026-09-15 22:10:19 +0900

    ✨ Add custom hooks to use region report api

--- apps/web/src/hooks/useProvincesQuery.ts
+++ apps/web/src/hooks/useProvincesQuery.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchProvinces } from '../api/regions';
+import { fetchProvinces } from '../api/regions/list';
 
 export default function useProvincesQuery() {
   return useQuery({
